bd8ffa5031f2319983d265f85da6d109447b557e
AlekseyKaramyshev <60324845+AlekseyKaramyshev@users.noreply.github.com>
2026-01-01 13:13:14 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#/bin/bash -eu
+# You must be privileged to run this
+
+GITHUB_USER_NAME="AlekseyKaramyshev"
+GITHUB_PROJECT_NAME="shvirtd-example-python"
+GITHUB_PROJECT_FULL_URL="https://github.com/${GITHUB_USER_NAME}/${GITHUB_PROJECT_NAME}.git"
+TARGET_DIR="/opt/${GITHUB_PROJECT_NAME}"
+
+function download_project {
+    echo "Downloading project: ${GITHUB_PROJECT_NAME}..."
+    /usr/bin/git clone "${GITHUB_PROJECT_FULL_URL}" "${TARGET_DIR}"
+    echo "Done downloading project: ${GITHUB_PROJECT_NAME}..."
+}
+
+function check_target_dir {
+    echo "Starting project: ${GITHUB_PROJECT_NAME}..."
+    /usr/bin/docker compose -f "${TARGET_DIR}/compose.yaml" up -d
+    echo "Done starting project: ${GITHUB_PROJECT_NAME}..."
+}
+
+function main {
+    download_project
+    check_target_dir
+}
+
+main
